7d89ca8279ede6ff87c3f217723018747999351f
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-06 21:36:02 +0600

    dynamic route

---
+++ src/app/market/[...slug]/page.jsx
@@ -0,0 +1,13 @@
+import Wrapper from "@/components/wrapper";
+
+const Market = ({ params }) => {
+  console.log(params.slug);
+
+  return (
+    <Wrapper>
+      <p>slug</p>
+    </Wrapper>
+  );
+};
+
+export default Market;
