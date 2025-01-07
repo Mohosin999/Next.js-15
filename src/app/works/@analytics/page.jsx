1634d5241bdf235d9258d79b99269719c6fbd4f4
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-07 14:32:25 +0600

    parallel routing

---
+++ src/app/works/@analytics/page.jsx
@@ -0,0 +1,16 @@
+import Wrapper from "@/components/wrapper";
+
+export default function Analytics() {
+  return (
+    <Wrapper>
+      <ul>
+        <li>
+          <a href="/dashboard/section1">Section 1</a>
+        </li>
+        <li>
+          <a href="/dashboard/section2">Section 2</a>
+        </li>
+      </ul>
+    </Wrapper>
+  );
+}
---
+++ src/app/works/page.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "@/components/wrapper";
+
+export default function Works() {
+  return (
+    <Wrapper>
+      <h1>Welcome to Works</h1>
+      <p>Select a team or analytics section to continue.</p>
+    </Wrapper>
+  );
+}
